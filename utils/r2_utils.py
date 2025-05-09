0c75abf470f9696b04011b91ba36fe71f82095d7
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2025-05-09 00:40:24 -0300

    Update r2_utils.py

--- utils/r2_utils.py
+++ utils/r2_utils.py
@@ -1,13 +1,12 @@
 import boto3
 from botocore.client import Config
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv()
+import os
 
-R2_KEY = os.getenv("R2_KEY")
-R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
-ENDPOINT_URL = os.getenv("ENDPOINT_URL")
-BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
+R2_KEY = os.environ.get("R2_KEY")
+R2_SECRET_KEY = os.environ.get("R2_SECRET_KEY")
+ENDPOINT_URL = os.environ.get("ENDPOINT_URL")
+BUCKET_NAME = os.environ.get("R2_BUCKET_NAME")
 
 
 def get_r2_client(R2_KEY, R2_SECRET_KEY, ENDPOINT_URL):
